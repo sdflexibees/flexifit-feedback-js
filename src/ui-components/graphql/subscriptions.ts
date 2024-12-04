5f161ba3a5a04e22e93ab6d76821f4c93220ba6c
Sowmya <sowmya.dhandapani@flexibees.com>
2024-12-04 18:39:17 +0530

    Update Refactor

---
+++ src/ui-components/graphql/subscriptions.ts
@@ -0,0 +1,58 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateFlexifitFeedback = /* GraphQL */ `
+  subscription OnCreateFlexifitFeedback(
+    $filter: ModelSubscriptionFlexifitFeedbackFilterInput
+  ) {
+    onCreateFlexifitFeedback(filter: $filter) {
+      candidateId
+      candidateLocation
+      candidateName
+      comment
+      createdAt
+      id
+      interviewDate
+      interviewer
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteFlexifitFeedback = /* GraphQL */ `
+  subscription OnDeleteFlexifitFeedback(
+    $filter: ModelSubscriptionFlexifitFeedbackFilterInput
+  ) {
+    onDeleteFlexifitFeedback(filter: $filter) {
+      candidateId
+      candidateLocation
+      candidateName
+      comment
+      createdAt
+      id
+      interviewDate
+      interviewer
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateFlexifitFeedback = /* GraphQL */ `
+  subscription OnUpdateFlexifitFeedback(
+    $filter: ModelSubscriptionFlexifitFeedbackFilterInput
+  ) {
+    onUpdateFlexifitFeedback(filter: $filter) {
+      candidateId
+      candidateLocation
+      candidateName
+      comment
+      createdAt
+      id
+      interviewDate
+      interviewer
+      updatedAt
+      __typename
+    }
+  }
+`;
