1765dfa7a1d56dd06a5a4f1a8fdde1551a9e6fd7
Sowmya <sowmya.dhandapani@flexibees.com>
2024-12-05 09:25:49 +0530

    Removing unwanted template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import type { FSchema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import {FlexifitFeedbackCreateForm } from "./ui-components" ;
@@ -7,19 +7,15 @@ import {FlexifitFeedbackCreateForm } from "./ui-components" ;
 const client = generateClient<FSchema>();
 
 function App() {
-  const [todos, setFlexiFitFeedbacks] = useState<Array<FSchema["FlexifitFeedback"]["type"][]>>([]);
+  // const [todos, setFlexiFitFeedbacks] = useState<Array<FSchema["FlexifitFeedback"]["type"][]>>([]);
 
   useEffect(() => {
     client.models.FlexifitFeedback.observeQuery().subscribe({
-      next: (data: { items: any; }) => setFlexiFitFeedbacks([...data.items]),
+      // next: (data: { items: any; }) => setFlexiFitFeedbacks([...data.items]),
     });
   }, []);
 
-  function createFlexiFitFeedback() {
-    console.log("Creating function")
-    // client.models.FlexifitFeedback.create();
-  }
-
+  
   return (
     <main>
       <h1>My Feedback</h1>
